740939a7f50975f171b63c137d4bcc8c15eff0c7
TalonExe <110237665+TalonExe@users.noreply.github.com>
2024-07-26 23:49:41 +0800

    hh

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-row'>
+      <Layout>
+        Hi
+      </Layout>
+    </div>
+  )
+}
+
+export default page
