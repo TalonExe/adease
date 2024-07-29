b1e5d33ffba5ba97aa1173087a5b01e4fc5ddbef
TalonExe <110237665+TalonExe@users.noreply.github.com>
2024-07-29 10:46:49 +0800

    minor changes

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -5,18 +5,17 @@ import React, { useState } from 'react'
 import Dnd from "@/components/Dnd"
 
 const page = () => {
-  const [clear, setClear] = useState(false)
   
   return (
     <Layout>
       <div className="p-4 px-8 my-4 w-full h-full">
         <div className='flex flex-row w-full justify-between mb-4'>
           <span className="font-sans font-bold text-2xl p-2">Landing Page Generator</span>
-        <button  className='h-fit w-fit p-2 bg-black text-white rounded-md' onClick={() => setClear(true)}>
+        <button  className='h-fit w-fit p-2 bg-black text-white rounded-md' >
           Clear Draggable
         </button>
         </div>
-        <Dnd clear={clear}/>
+        <Dnd/>
       </div>
     </Layout>
   )
--- src/components/Dnd/index.tsx
+++ src/components/Dnd/index.tsx
@@ -1,58 +1,94 @@
-import React, {useState} from 'react';
-import {DndContext, DragEndEvent, DragMoveEvent, UniqueIdentifier} from '@dnd-kit/core';
+import React, { useState } from "react";
+import {
+  DndContext,
+  DragEndEvent,
+  DragMoveEvent,
+  DragStartEvent,
+  UniqueIdentifier,
+} from "@dnd-kit/core";
 
-import {Droppable} from '@/components/ui/Droppable';
-import {Draggable} from '@/components/ui/Draggable';
+import { Droppable } from "@/components/ui/Droppable";
+import { Draggable } from "@/components/ui/Draggable";
+
+type markup = {
+  id: UniqueIdentifier;
+  element: React.ReactNode;
+  parent: UniqueIdentifier | undefined;
+};
 
 function Dnd() {
-  const containers = ['A', 'B', 'C'];
+  const containers = ["A", "B", "C"];
   const [parent, setParent] = useState<UniqueIdentifier>();
-  const [child, setChild] = useState()
-  
-  const draggableMarkup = (
-    [{
+  const [child, setChild] = useState<markup[]>([
+    { id: "", element: <></>, parent: "" },
+  ]);
+  const [buttonList, setButtonList] = useState<markup[]>([
+    {
       id: 1,
-      element:<Draggable key="1" id="1">Drag me</Draggable>,
-      parent: 'A'
+      element: (
+        <Draggable key="1" id="1">
+          Drag me
+        </Draggable>
+      ),
+      parent: "Z",
     },
     {
       id: 2,
-      element:<Draggable key="2" id="2">Drag me</Draggable>,
-      parent: 'A'
+      element: (
+        <Draggable key="2" id="2">
+          Drag me
+        </Draggable>
+      ),
+      parent: "Z",
     },
     {
       id: 3,
-      element:<Draggable key="3" id="3">Drag me</Draggable>,
-      parent: 'A'
+      element: (
+        <Draggable key="3" id="3">
+          Drag me
+        </Draggable>
+      ),
+      parent: "Z",
     },
-    ]
-      
-    
-  );
+  ]);
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {parent === undefined ? draggableMarkup.map((d)=> d.element) : undefined}
+    <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
+      <Droppable key="Z" id="Z">
+        {buttonList.filter((d) => d.parent == "Z").map((d) => d.element)}
+      </Droppable>
 
       {containers.map((id) => (
         // We updated the Droppable component so it would accept an `id`
         // prop and pass it to `useDroppable`
+
         <Droppable key={id} id={id}>
-          {parent === id ? draggableMarkup.map((d)=> d.element) : 'Drop here'}
+          Drag Here
+          {buttonList.filter((d) => d.parent == id).map((d) => d.element)}
         </Droppable>
       ))}
     </DndContext>
   );
 
-  function handleDragEnd(event : DragEndEvent) {
-    const {over} = event;
-
-    // If the item is dropped over a container, set it as the parent
-    // otherwise reset the parent to `null`
-    console.log(over);
-    
-    setParent(over ? over.id : undefined);
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event;
+    setChild(
+      buttonList.filter((d) => {
+        return d.id == active.id;
+      })
+    );
+    console.log(child[0]);
   }
-};
 
-export default Dnd
+  function handleDragEnd(event: DragEndEvent) {
+    const { over } = event;
+    setParent(over ? over.id : undefined);
+    buttonList
+      .filter((d) => {
+        return d.id == child[0].id;
+      })
+      .map((d) => (d.parent = parent));
+  }
+}
+
+export default Dnd;
